68b64de6add7987930553f41558cdc1bfbe24d05
Mona Lisa <naomiroznik@gmail.com>
2022-12-01 15:53:05 -0500

    updating handlebars

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-const path = require('path');const exphbs = require('express-handlebars');
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers/');
 const hbs = exphbs.create({});
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
