4f09015c0986d4e549c1333c4590b28b74b15118
Mona Lisa <naomiroznik@gmail.com>
2022-12-02 14:21:39 -0500

    finally fixed the problem with public/stylesheets folder

---
+++ public/javascript/add-post.js
@@ -0,0 +1,26 @@
+// module 14.5.4
+async function newFormHandler(event) {
+    event.prventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post_text = document.querySelector('input[name="post-text"]').value;
+
+    const response = await fetch('/api/post', {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            post_text
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
