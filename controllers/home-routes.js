33eba098a44f22b7436a966664d5e691a7c27895
Mona Lisa <naomiroznik@gmail.com>
2022-12-02 11:04:10 -0500

    fixing home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,17 +1,3 @@
-// router.get('/', (req, res) => {
-//     res.render('homepage', {
-//       id: 1,
-//       post_url: 'https://handlebarsjs.com/guide/',
-//       title: 'Handlebars Docs',
-//       created_at: new Date(),
-//       vote_count: 10,
-//       comments: [{}, {}],
-//       user: {
-//         username: 'test_user'
-//       }
-//     });
-//   });
-
 // module 14.3.3
 const router = require('express').Router();
 const sequelize = require('../config/connection');
@@ -27,7 +13,7 @@ router.get('/', (req, res) => {
       'id',
       'post_url',
       'title',
-      'created_at',
+      'created_at'
     ],
     include: [
       {
@@ -47,7 +33,56 @@ router.get('/', (req, res) => {
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
 
-      res.render('homepage', { posts });
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// get single post
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+
+      const post = dbPostData.get({ plain: true });
+
+      res.render('single-post', {
+        post,
+        loggedIn: req.session.loggedIn
+      });
     })
     .catch(err => {
       console.log(err);
@@ -56,6 +91,11 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
   res.render('login');
 });
 
