e1065fc9d509a140d1b9a5c80b003795303fc529
Mona Lisa <naomiroznik@gmail.com>
2022-12-02 13:19:50 -0500

    refining server.js

--- server.js
+++ server.js
@@ -1,45 +1,8 @@
-// const path = require('path');
-// const express = require('express');
-// const session = require('express-session');
-// const exphbs = require('express-handlebars');
-// const routes = require('./controllers/');
-// // const hbs = exphbs.create({});
-
-// const sequelize = require('./config/connection');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
-
-
-// app.use(session(sess));
-
-// const hbs = exphbs.create({ helpers });
-
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use(require('./controllers/'));
-
-
-// sequelize.sync({ force: false }).then(() => {
-//     app.listen(PORT, () => console.log('Now listening'));
-//   });
-
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
-
 const helpers = require('./utils/helpers');
-const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -47,8 +10,22 @@ const PORT = process.env.PORT || 3001;
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const hbs = exphbs.create({ helpers });
+
 // const hbs = exphbs.create({});
- 
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
